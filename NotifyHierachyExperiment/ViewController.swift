50094703c80459c39c00f93e5f373d643811178c
Mark Cornelisse <m.p.cornelisse@gmail.com>
2024-03-04 10:22:15 +0100

    Initial Commit

---
+++ NotifyHierachyExperiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotifyHierachyExperiment
+//
+//  Created by Mark Cornelisse on 04/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
